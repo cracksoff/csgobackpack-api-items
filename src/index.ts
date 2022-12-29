1c3910462bbb16246f85c78699647e389d0cd791
cracksoff <59627202+cracksoff@users.noreply.github.com>
2022-12-29 19:22:52 +0300

    divided into two applications

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import process from 'node:process'
 import { server } from '#app/routes/index.js'
 import logger from '#app/utils/log.js'
-import { startParser } from '#app/csgobackpack/index.js'
 
 await server.listen({
 	port: Number(process.env.SERVER_PORT ?? 8080),
@@ -12,5 +11,3 @@ logger.info({
 	message: 'server started',
 	port: Number(process.env.SERVER_PORT ?? 8080),
 })
-
-await startParser()
